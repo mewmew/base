35530e32cafc7e47c8e140e48fc4df44abb9c9c1
mewmew <random0x00@gmail.com>
2012-09-30 19:39:48 +0200

    sort: Added sort command.

---
+++ sort/README.md
@@ -0,0 +1,16 @@
+sort
+====
+
+sort is a tool which writes the sorted concatenation of all provided files to
+standard output. If no file is provided it will read from standard input.
+
+Installation
+------------
+
+    $ go get github.com/mewmew/gobase/sort
+
+Usage
+-----
+
+    $ sort file1.txt file2.txt
+    $ sort < file1.txt
---
+++ sort/sort.go
@@ -0,0 +1,55 @@
+package main
+
+import "flag"
+import "fmt"
+import "log"
+import "os"
+import "sort"
+
+import "github.com/mewkiz/pkg/bufioutil"
+
+func init() {
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage: sort [OPTION]... [FILE]...")
+	fmt.Fprintln(os.Stderr, "Write sorted concatenation of all FILE(s) to standard output.")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "With no FILE, or when FILE is -, read standard input.")
+	fmt.Fprintln(os.Stderr)
+	flag.PrintDefaults()
+}
+
+func main() {
+	err := Sort(flag.Args())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Sort writes the sorted concatenation of all provided files or standard input
+// (when no file has been provided).
+func Sort(filePaths []string) (err error) {
+	lines := make([]string, 0)
+	for _, filePath := range filePaths {
+		l, err := bufioutil.ReadLines(filePath)
+		if err != nil {
+			return err
+		}
+		lines = append(lines, l...)
+	}
+	if len(filePaths) == 0 {
+		br := bufioutil.NewReader(os.Stdin)
+		lines, err = br.ReadLines()
+		if err != nil {
+			return err
+		}
+	}
+	sort.Strings(lines)
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+	return nil
+}
